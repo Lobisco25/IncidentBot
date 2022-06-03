fddb41f0c0367ee449c48924bb1751c3f68d23c4
Bytter <67333597+Byttter@users.noreply.github.com>
2022-06-02 21:21:30 -0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,31 +1,22 @@
 exports.run = (client, args, channel, tags, message) => {
-    function format(time){
-        let day = time / 86400
-        let hours = time / 3600
+    function format(time) {
+        let days = Math.floor(time / 86400)
+        let hours = Math.floor(time / 3600)
         let minutes = Math.floor(time / 60)
-        
-        console.log(day, hours, minutes)
-        switch(time) {
-            case time >= 60 || time <= 3600:
-                return minutes
-                break;
-            default:
-                console.log("xddddd")
-        }
-
     
+        return `${('0' + days).slice(-2)}:${('0' + hours).slice(-2)}:${('0' + minutes).slice(-2)}:${('0' + time).slice(-2)}`
     }
 
     var uptime = Math.floor(process.uptime())
-    console.log(uptime)
-    format(uptime)
+    // console.log(uptime)
+    // format(80)
     const used = process.memoryUsage().heapUsed / 1024 / 1024
     const memoria = Math.round(used * 100) / 100
 
     client.ping().then(function(data) {
         let ping = Math.floor(Math.round(data * 1000))
         
-        // client.say(channel, `@${tags.username} PONG! Ping: ${ping}ms | Uptime: ${format(uptime)} | Memória: ${memoria}MB/512MBs`)
+        client.say(channel, `@${tags.username} PONG! Ping: ${ping}ms | Uptime: ${format(uptime)} | Memória: ${memoria}MB/512MBs`)
     })
 }
 module.exports.config = {
