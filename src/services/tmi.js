d2caf1e028d0940b031a57046abd927e870ad960
Lobisco25 <fefeeliaslobo@outlook.com>
2022-06-11 22:16:43 -0300

    Fixing fs readdir for handlers

--- src/services/tmi.js
+++ src/services/tmi.js
@@ -14,7 +14,7 @@ client.connect().catch(err => console.log(errc));
 client.on("connected", (adress, port) => {
     client.ping().then(function (data) {
         let ping = Math.floor(Math.round(data * 1000))
-        client.say("botirinho", `TrollDespair fui reinciado (${ping}ms)`)
+        client.say("bytter_", `TrollDespair fui reinciado (${ping}ms)`)
     })
 })
 
