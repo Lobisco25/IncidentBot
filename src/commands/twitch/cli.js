2bea489411aa97d98b1bddf9569c0e763e5e5ba2
Lobisco25 <fefeeliaslobo@outlook.com>
2022-06-15 18:00:10 -0300

    cli agora funciona com id e nome do usuário

--- src/commands/twitch/cli.js
+++ src/commands/twitch/cli.js
@@ -4,14 +4,26 @@ exports.run = async (client, args, channel, tags, message, user) => {
     if (!args[0])
         return
 
-    const search = await twurple.users.getUserById(args[0])
-    client.say(
-        channel,
-        `@${search.displayName} | ${search.id} | Criado em: ${search.creationDate} | Bio: ${search.description} | Foto de Perfil: ${search.profilePictureUrl}`
-    )
+    switch (isNaN(args[0])) {
+        case (true):
+            search = await twurple.users.getUserByName(args[0])
+            client.say(
+            channel,
+            `@${search.displayName} | ${search.id} | Criado em: ${search.creationDate} | Bio: ${search.description} | Foto de Perfil: ${search.profilePictureUrl}`
+            )
+            break;
+        case (false):
+            search = await twurple.users.getUserById(args[0])
+            client.say(
+            channel,
+            `@${search.displayName} | ${search.id} | Criado em: ${search.creationDate} | Bio: ${search.description} | Foto de Perfil: ${search.profilePictureUrl}`
+            )
+            break
+        }
+
 }
 module.exports.config = {
     name: "cli",
-    description: "",
+    description: "Pega as informações de um usuário da Twitch.",
     aliases: ["user"],
 }
