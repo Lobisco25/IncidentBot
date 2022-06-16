b604219ab83727794f839a119fdbd83c2342a090
Lobisco25 <fefeeliaslobo@outlook.com>
2022-06-15 23:36:44 -0300

    Mudança na formatação de data de conta no cli

--- src/commands/twitch/cli.js
+++ src/commands/twitch/cli.js
@@ -7,16 +7,18 @@ exports.run = async (client, args, channel, tags, message, user) => {
     switch (isNaN(args[0])) {
         case (true):
             search = await twurple.users.getUserByName(args[0])
+            creationDate = `${search.creationDate.getUTCDate()}/${search.creationDate.getUTCMonth() + 1}/${search.creationDate.getUTCFullYear()} as ${search.creationDate.getHours()}:${search.creationDate.getMinutes()}:${search.creationDate.getSeconds()}`
             client.say(
             channel,
-            `@${search.displayName} | ${search.id} | Criado em: ${search.creationDate} | Bio: ${search.description} | Foto de Perfil: ${search.profilePictureUrl}`
+            `@${search.displayName} | ${search.id} | Criado em: ${creationDate} | Bio: ${search.description} | Foto de Perfil: ${search.profilePictureUrl}`
             )
             break;
         case (false):
             search = await twurple.users.getUserById(args[0])
+            creationDate = `${search.creationDate.getUTCDate()}/${search.creationDate.getUTCMonth() + 1}/${search.creationDate.getUTCFullYear()} as ${search.creationDate.getHours()}:${search.creationDate.getMinutes()}:${search.creationDate.getSeconds()}`
             client.say(
             channel,
-            `@${search.displayName} | ${search.id} | Criado em: ${search.creationDate} | Bio: ${search.description} | Foto de Perfil: ${search.profilePictureUrl}`
+            `@${search.displayName} | ${search.id} | Criado em: ${creationDate} | Bio: ${search.description} | Foto de Perfil: ${search.profilePictureUrl}`
             )
             break
         }
