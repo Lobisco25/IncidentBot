4e68384e953c925baf12506342d54b0a9ff91a72
Lobisco25 <fefeeliaslobo@outlook.com>
2022-06-17 00:46:54 -0300

    Novo comando (adminhelp)

---
+++ src/commands/twitch/adminhelp.js
@@ -0,0 +1,16 @@
+exports.run = (client, args, channel, tags, message, user) => {
+    const commandsName = Object.keys(client.commands)
+    var result = []
+    commandsName.forEach(cmdName => {
+        if(client.commands[cmdName].config.adminOnly){
+            result.push(client.commands[cmdName].config.name)
+        }
+    })
+    client.say(channel, `Os comando de admin são: ${result.join(", ")} :)`)
+}
+module.exports.config = {
+    name: "adminhelp",
+    description: "manda os comandos de admin",
+    aliases: ["admin", "admincommands"],
+    adminOnly: true,
+}
