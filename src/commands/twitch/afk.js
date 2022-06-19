95235d075ae6a7f1c4c125260c2629229e2a4a6a
Bytter <67333597+Byttter@users.noreply.github.com>
2022-06-19 13:54:52 -0300

    aliases no afk(Incompleto)

--- src/commands/twitch/afk.js
+++ src/commands/twitch/afk.js
@@ -7,25 +7,26 @@ async function setAFKStatus(message, tags) {
     )
 }
 exports.run = async (client, args, channel, tags, message, user) => {
-    if (!args[0]) {
+    const alias = this.config.aliases
+    const a = args[0] === undefined ? "(Sem mensagem)" : `${args.join(" ")}`
+    if(alias.includes("afk") === true) {
         await setAFKStatus("(sem mensagem)", tags)
         await client.say(
             channel,
-            `${tags.username} agora está AFK: (sem mensagem)`
+            `${tags.username} agora está AFK: ${a}`
         )
-    }
-    else {
+    } else if(alias.includes("gn") === true) {
         await setAFKStatus(args.join(" "), tags)
-        await client.say(
-            channel,
-            `${tags.username} agora está AFK: ${args.join(" ")}`
-        )
+        await client.say(channel, `${tags.username} foi dormir: ${a}`)
+    } else if(alias.includes("code") === true) {
+        await setAFKStatus(args.join(" "), tags)
+        await client.say(channel, `${tags.username} foi programar: ${a}`)
     }
 }
 module.exports.config = {
     name: "afk",
     description:
         "Avisa ao chat que você vai ficar longe ou algo desse tipo eu n sei como as pessoa realmente usam o afk",
-    aliases: [""],
+    aliases: ["afk ","gn", "code"],
     cooldown: 10000
 }
