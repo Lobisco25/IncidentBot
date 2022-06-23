d93e3b98a8d486c5205509a10f012f958ae12091
Bytter <67333597+Byttter@users.noreply.github.com>
2022-06-23 18:28:27 -0300

    Foi adicionado toLowerCase() no user.remind

--- src/handlers/remind.js
+++ src/handlers/remind.js
@@ -4,7 +4,8 @@ const UserModel = require("../models/User")
 client.on("message", async (channel, tags, message, self) => {
     try {
         const user = await UserModel.findOne({ to: tags["username"] })
-        if (user.remind[0].to == tags["username"]) {
+        // console.log(user.remind[0].to., tags["username"])
+        if (user.remind[0].to.toLowerCase() == tags["username"]) {
             await client.say(
                 channel,
                 `${user.remind[0].to}, Você tem um novo remind de ${user.remind[0].from}: ${user.remind[0].message}`
