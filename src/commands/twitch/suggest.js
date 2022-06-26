a831877ebe42a9f4f73b1d4b5f356e659278a3a7
Bytter <67333597+Byttter@users.noreply.github.com>
2022-06-26 11:14:24 -0300

    novas sugest

--- src/commands/twitch/suggest.js
+++ src/commands/twitch/suggest.js
@@ -1,12 +1,28 @@
 const SuggestModel = require("../../models/Suggest.js")
+const { MessageEmbed } = require("discord.js")
+const discordClient = require("../../services/discord")
 const log = require("../../handlers/logger")
 
 exports.run = async (client, args, channel, tags, message, user) => {
-    await SuggestModel.create({
-        author_name: tags.username,
-        message: args.join(" "),
-    }).catch(err => { log.error("Erro ao criar uma sugestão ", err) })
-    client.say(channel, `pajaH A sua sugestão foi anotada!`)
+    await SuggestModel.create(
+        {
+            author_name: tags.username,
+            message: args.join(" "),
+        },
+        function (err, i) {
+            if (err) log.error("Erro ao criar uma sugestão", err)
+            client.say(channel, "pajaH A sua sugestão foi anotada!")
+
+            const embed = new MessageEmbed()
+                .setColor("#fc1303")
+                .setTitle(`${i.author_name}`)
+                .setDescription(`sugestão: ${i.message}`)
+
+            discordClient.channels.cache
+                .get("983065628984369244")
+                .send({ embeds: [embed] })
+        }
+    )
 }
 
 module.exports.config = {
