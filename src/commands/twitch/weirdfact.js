6d1678822a694b5adc989bb3f540983e1b44eb00
Bytter <67333597+Byttter@users.noreply.github.com>
2022-07-14 14:04:18 -0300

    Novo comando weirdfact

---
+++ src/commands/twitch/weirdfact.js
@@ -0,0 +1,31 @@
+const axios = require("axios")
+const cheerio = require("cheerio")
+const translate = require("translate-google")
+
+exports.run = async (client, args, channel, tags, message, user) => {
+        axios({
+            method: "GET",
+            url: "https://www.bestrandoms.com/random-weird-facts"
+        })
+
+        .then(async (res) => {
+            const $ = cheerio.load(res.data)
+            const movies = $(".content .list-unstyled").map((i, e) => {
+                const fact = $($(e).children()[1]).text()
+                return { fact }
+            }) 
+
+            const translation =  await translate(movies[0].fact.replace("[Amazing]", " "), {from: 'en', to: 'pt'})
+            client.say(
+                channel,
+                `${translation.replace("[Incrível]", " ")}`
+            )
+
+})
+}
+module.exports.config = {
+    name: "weirdfact",
+    description: "Retorna um fato aleatório",
+    aliases: ["fatos"],
+    cooldown: 5000,
+}
