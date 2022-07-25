d8e2506b5a5e795e1f6c8728f32506c1d5a145fd
Lobisco25 <fefeeliaslobo@outlook.com>
2022-07-25 14:25:22 -0300

    finalizando o restart

--- src/commands/twitch/restart.js
+++ src/commands/twitch/restart.js
@@ -3,8 +3,8 @@ const { exec } = require("child_process");
 exports.run = async (client, args, channel, tags, message, user) => {
     await client.say(channel, "RareParrot rodando git pull")
     await exec("git pull")
-    await client.say(channel, 'ppCircle reinciando')
-    process.exit()
+    await client.say(channel, 'ApuApustaja TeaTime reinciando')
+    await process.exit()
 }
 module.exports.config = {
     name: "restart",
