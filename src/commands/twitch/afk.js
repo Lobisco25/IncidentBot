f518a7c5f77f6a366cce2b646bd90fefd2f46a46
Bytter <67333597+Byttter@users.noreply.github.com>
2022-07-27 19:29:57 -0300

    Usando objetos para deixar o codigo mais limpo

--- src/commands/twitch/afk.js
+++ src/commands/twitch/afk.js
@@ -4,55 +4,40 @@ async function setAFKStatus(message, tags, type) {
         afk: {
             message: message,
             time: new Date(),
-            _type: type
-        }
+            _type: type,
+        },
     }
 
-    await UserModel.findOneAndUpdate(
-        { twitch_id: tags["user-id"] },
-        query,
-        { upsert: true, new: true, setDefaultsOnInsert: true }
-    )
+    await UserModel.findOneAndUpdate({ twitch_id: tags["user-id"] }, query, {
+        upsert: true,
+        new: true,
+        setDefaultsOnInsert: true,
+    })
 }
+
 exports.run = async (client, args, channel, tags, message, user) => {
     const msg = args[0] === undefined ? "(Sem mensagem)" : `${args.join(" ")}`
     await setAFKStatus(msg, tags, tags.source)
     var output = null
-    switch (tags.source) {
-        case "afk":
-            output = `${tags.username} agora está AFK: ${msg}`
-            break
-        case "study":
-            output = `${tags.username} foi estudar: ${msg}`
-            break
-        case "code":
-            output = `${tags.username} foi programar: ${msg}`
-            break
-        case "gn":
-            output = `${tags.username} foi dormir: ${msg}`
-            break
-        case "workout":
-            output = `${tags.username} foi malhar: ${msg}`
-            break
-        case "shower": 
-            output = `${tags.username} foi tomar banho: ${msg}`
-            break
-        case "brb":
-            output = `${tags.username} volta já: ${msg}`
-            break
-        case "work":
-            output = `${tags.username} foi trabalhar: ${msg}`
-            break
-        case "read":
-            output = `${tags.username} foi ler: ${msg}`
-            break
-        case "food": 
-            output = `${tags.username} foi comer: ${msg}`
-            break
-        case "fuck":
-            output = `${tags.username} foi foder: ${msg}`
-            break
+    const afkType = (type) => {
+        const source = {
+            afk: `${tags.username} agora está AFK: ${msg}`,
+            brb: `${tags.username} volta já: ${msg}`,
+            gn: `${tags.username} foi dormir: ${msg}`,
+            code: `${tags.username} foi programar: ${msg}`,
+            work: `${tags.username} foi trabalhar: ${msg}`,
+            study: `${tags.username} foi estudar: ${msg}`,
+            workout: `${tags.username} foi malhar: ${msg}`,
+            read: `${tags.username} foi ler: ${msg}`,
+            food: `${tags.username} foi comer: ${msg}`,
+            fuck: `${tags.username} foi foder: ${msg}`,
+            shower: `${tags.username} foi tomar banho: ${msg}`,
+        }
+        output = source[type]
+        return output
     }
+
+    afkType(tags.source)
     await client.say(channel, output)
 }
 
@@ -60,6 +45,6 @@ module.exports.config = {
     name: "afk",
     description:
         "Avisa ao chat que você vai ficar longe ou algo desse tipo eu n sei como as pessoa realmente usam o afk",
-    aliases: ["gn", "study", "code", "workout", "shower", "brb", "work", "read", "food", "fuck"],
-    cooldown: 2000
+    aliases: ["gn","study","code","workout","shower","brb","work","read","food","fuck"],
+    cooldown: 2000,
 }
--- src/handlers/afk.js
+++ src/handlers/afk.js
@@ -2,53 +2,35 @@ const client = require("../services/tmi")
 const UserModel = require("../models/User")
 const prettyMilliseconds = require("pretty-ms")
 
-    client.on("message", async (channel, tags, message, self) => {
-        const user = await UserModel.findOne({twitch_id: tags["user-id"]})
-        if(!user) return
-        if(!user.afk?.time) return
+client.on("message", async (channel, tags, message, self) => {
+    const user = await UserModel.findOne({ twitch_id: tags["user-id"] })
+    if (!user) return
+    if (!user.afk?.time) return
     
-    
-        const time = prettyMilliseconds(Date.now() - user.afk.time)
-        
-        var output = null
-        switch (user.afk._type) {
-            case "afk":
-                output = `${tags.username} voltou de seu AFK: ${user.afk.message} (${time})`
-                break
-            case "study":
-                output = `${tags.username} terminou de estudar: ${user.afk.message} (${time})`
-                break
-            case "code":
-                output = `${tags.username} terminou de programar: ${user.afk.message} (${time})`
-                break
-            case "gn":
-                output = `${tags.username} acordou: ${user.afk.message} (${time})`
-                break
-            case "workout":
-                output = `${tags.username} acabou de malhar: ${user.afk.message} (${time})`
-                break 
-            case "shower":
-                output = `${tags.username} acaba de se banhar: ${user.afk.message} (${time})`
-                break
-            case "brb":
-                output = `${tags.username} voltou: ${user.afk.message} (${time})`
-                break
-            case "work":
-                output = `${tags.username} voltou do trabalho: ${user.afk.message} (${time})`
-                break
-            case "read":
-                output = `${tags.username} terminou de ler: ${user.afk.message} (${time})`
-                break
-            case "food":
-                output = `${tags.username} está com a barriga cheia: ${user.afk.message} (${time})`
-                break
-            case "fuck":
-                output = `${tags.username} terminou a sua foda: ${user.afk.message} (${time})`
-                break
-        }
-        await client.say(channel, output)
-    
-        user.afk = undefined
-        user.save()
-    })
+    const time = prettyMilliseconds(Date.now() - user.afk.time)
+    var output = null
 
+    const afkType = (type) => {
+        const source = {
+            brb: `${tags.username} voltou: ${user.afk.message} (${time})`,
+            gn: `${tags.username} acordou: ${user.afk.message} (${time})`,
+            afk: `${tags.username} voltou de seu AFK: ${user.afk.message} (${time})`,
+            workout: `${tags.username} acabou de malhar: ${user.afk.message} (${time})`,
+            code: `${tags.username} terminou de programar: ${user.afk.message} (${time})`,
+            work: `${tags.username} voltou do trabalho: ${user.afk.message} (${time})`,
+            study: `${tags.username} terminou de estudar: ${user.afk.message} (${time})`,
+            read: `${tags.username} terminou de ler: ${user.afk.message} (${time})`,
+            food: `${tags.username} está com a barriga cheia: ${user.afk.message} (${time})`,
+            fuck: `${tags.username} terminou a sua foda: ${user.afk.message} (${time})`,
+            shower: `${tags.username} acaba de se banhar: ${user.afk.message} (${time})`,
+        }
+        output = source[type]
+        return output
+    }
+
+    afkType(user.afk._type)
+    await client.say(channel, output)
+
+    user.afk = undefined
+    user.save()
+})
