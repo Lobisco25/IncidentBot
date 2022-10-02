e9e3796c3aa4539a47ff33264fced06858fb9f30
Lobisco25 <fefeeliaslobo@outlook.com>
2022-10-02 15:45:26 -0300

    esqueci do ) no final xd

--- src/commands/twitch/restart.js
+++ src/commands/twitch/restart.js
@@ -1,7 +1,7 @@
 const { exec } = require("child_process");
 
 exports.run = async (client, args, channel, tags, message, user) => {
-    await client.say(channel, "Restarting ApuApustaja TeaTime (git pull -> pnpm i")
+    await client.say(channel, "Restarting ApuApustaja TeaTime (git pull -> pnpm i)")
     await exec("git pull")
     await exec("pnpm i")
     await process.exit()
