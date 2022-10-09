9b90134895dd66b0b07337e970ae8970cf28ab37
Bytter <67333597+Byttter@users.noreply.github.com>
2022-10-09 20:25:13 -0300

    o filme está arrumado

--- src/commands/twitch/filme.js
+++ src/commands/twitch/filme.js
@@ -3,28 +3,26 @@ const cheerio = require("cheerio")
 const translate = require("translate-google")
 
 exports.run = async (client, args, channel, tags, message, user) => {
-        axios({
-            method: "GET",
-            url: "https://www.bestrandoms.com/random-movie-generator"
+    axios({
+        method: "GET",
+        url: "https://www.bestrandoms.com/random-movie-generator",
+    }).then(async (res) => {
+        const $ = cheerio.load(res.data)
+        const movies = $(".content .list-unstyled li").map((i, e) => {
+            const name = $($(e).children()[2]).text()
+            const description = $($(e).children()[3]).text()
+
+            return { name, description }
         })
 
-        .then(async (res) => {
-            const $ = cheerio.load(res.data)
-            const movies = $(".content .list-unstyled li").map((i, e) => {
-                const name = $($(e).children()[1]).text()
-                const rating = $($(e).children()[2]).text()
-                const description = $($(e).children()[3]).text()
-
-                return { name, description, rating }
-            }) 
-
-            const translation =  await translate(movies[0].description, {from: 'en', to: 'pt'})
-
-            await client.say(
-                channel,
-                `Nome: ${movies[0].name}, descrição: "${translation}", ${movies[0].rating}`
-            )
+        const translation = await translate(movies[0].description, {
+            from: "en",
+            to: "pt",
         })
+
+        console.log(movies[0])
+        await client.say(channel, `Nome: ${movies[0].name}, "${translation}",`)
+    })
 }
 module.exports.config = {
     name: "filme",
