1db997fb734f62fe2d16d2df5af1a15a09cfaa3e
Bytter <67333597+Byttter@users.noreply.github.com>
2022-10-12 11:59:22 -0300

    Trailer no filme.js

--- src/commands/twitch/filme.js
+++ src/commands/twitch/filme.js
@@ -8,20 +8,27 @@ exports.run = async (client, args, channel, tags, message, user) => {
         url: "https://www.bestrandoms.com/random-movie-generator",
     }).then(async (res) => {
         const $ = cheerio.load(res.data)
-        const movies = $(".content .list-unstyled li").map((i, e) => {
+        const movie = $(".content .list-unstyled li").map((i, e) => {
             const name = $($(e).children()[2]).text()
+            const trailer = $($($(e).children()[5]).children()[0]).attr(
+                "data-trailer"
+            )
             const description = $($(e).children()[3]).text()
 
-            return { name, description }
+            return { name, description, trailer }
         })
 
-        const translation = await translate(movies[0].description, {
+        const translation = await translate(movie[0].description, {
             from: "en",
             to: "pt",
         })
 
-        console.log(movies[0])
-        await client.say(channel, `Nome: ${movies[0].name}, "${translation}",`)
+        const Trailer =
+            movie[0].trailer === "" ? "" : `trailer: https://www.youtube.com/watch?v=${movie[0].trailer}`
+        await client.say(
+            channel,
+            `Nome: ${movie[0].name}, ${translation}, ${Trailer}`
+        )
     })
 }
 module.exports.config = {
