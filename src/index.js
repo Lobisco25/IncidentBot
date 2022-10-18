ef23b30ee60b92f8705807021ae63bbb1e387f7f
Lobisco25 <fefeeliaslobo@outlook.com>
2022-10-18 02:19:17 -0300

    removendo handler do discord

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 require("dotenv").config()
 require("./handlers/tmi") // twitch handler
 require("./handlers/7tv.js").initialize() // initialize 7tv events
-require("./handlers/discord") // discord handler
 require("./services/discord.js")
 require("./handlers/afk.js")
 const axios = require("axios")
