f2e84d54ffbf0d29a40c9d1856de06ccd2a085c5
Lobisco25 <fefeeliaslobo@outlook.com>
2023-01-26 17:33:22 -0300

    git is so confusing

--- src/services/twurple.js
+++
@@ -1,7 +0,0 @@
-const { StaticAuthProvider } = require('@twurple/auth');
-const clientId = process.env.TWITCH_CLIENT_ID;
-const accessToken = process.env.TWITCH_ACESS_TOKEN
-const authProvider = new StaticAuthProvider(clientId, accessToken)
-const { ApiClient } = require('@twurple/api')
-const api = new ApiClient({ authProvider })
-module.exports = api
